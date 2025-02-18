7b2d29ce0611f8b90c8101708e5cab9e94423ce9
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-02-18 11:17:19 +0800

    Add urlencoded

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 
 const messages = [
     {
