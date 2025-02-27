4bd13d3090e77d7d3f0dc3e615fa795eb8c68a18
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-02-27 13:02:37 +0800

    Add express validator to validate form

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const appRouter = require("./routes/appRouter")
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use("/", appRouter);
+app.set("views", path.join(__dirname, "views"));
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
 
-const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ];
 
-  app.get("/", (req,res) => {
-    res.render("index", {title:"Mini Message Board", messages});
-  });
 
-  app.get("/new", (req,res) => {
-    res.render("form");
-  });
-
-  app.post("/new", (req, res) => {
-    const { messageText, messageUser } = req.body;
-    messages.push({ text: messageText, user: messageUser, added: new Date() });
-    res.redirect("/");
-  });
+ 
 
   app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
